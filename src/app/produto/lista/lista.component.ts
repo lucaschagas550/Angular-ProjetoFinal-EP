974fe2019e6e200ad4b9ad0ccb30295727e9f9db
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2024-10-11 23:10:46 -0300

    feat: unsubscribe com Ondestroy de forma correta

--- src/app/produto/lista/lista.component.ts
+++ src/app/produto/lista/lista.component.ts
@@ -1,26 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Produto } from '../models/produto';
 import { ProdutoService } from '../services/produto.service';
 import { environment } from 'src/environments/environment';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html'
 })
-export class ListaComponent implements OnInit {
+export class ListaComponent implements OnInit, OnDestroy {
 
   imagens: string = environment.imagensUrl;
 
+  produtosSubs !: Subscription;
   public produtos!: Produto[];
   errorMessage!: string;
 
   constructor(private produtoService: ProdutoService) { }
 
+
   ngOnInit(): void {
-    this.produtoService.obterTodos()
+    this.produtosSubs = this.produtoService.obterTodos()
       .subscribe({
         next: (produtos) => this.produtos = produtos,
         error: () => this.errorMessage
       });
+  }
+
+  ngOnDestroy(): void {
+    this.produtosSubs.unsubscribe();
   }
 }
